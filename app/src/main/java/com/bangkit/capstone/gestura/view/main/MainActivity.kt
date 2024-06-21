acfbf3c8dffac22c1291a3f1ef538923bbef20e1
shafanjw <shafa.najwa@ui.ac.id>
2024-06-21 14:50:26 +0700

    Main Feature FE, connected & models tested

--- app/src/main/java/com/bangkit/capstone/gestura/view/main/MainActivity.kt
+++ app/src/main/java/com/bangkit/capstone/gestura/view/main/MainActivity.kt
@@ -10,13 +10,13 @@ import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bangkit.capstone.gestura.R
 import com.bangkit.capstone.gestura.databinding.ActivityMainBinding
 import com.bangkit.capstone.gestura.view.ViewModelFactory
 import com.bangkit.capstone.gestura.view.detectmovement.CameraActivity
-import com.bangkit.capstone.gestura.view.login.LoginActivity
 import com.bangkit.capstone.gestura.view.welcome.WelcomeActivity
 import com.bumptech.glide.Glide
 
@@ -48,6 +48,13 @@ class MainActivity : AppCompatActivity() {
             true
         }
         setupView()
+
+        // Find the CardView and set an OnClickListener
+        val btnScan: CardView = findViewById(R.id.btnscan)
+        btnScan.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun isUserLoggedIn(): Boolean {
@@ -84,38 +91,9 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
     }
 
-<<<<<<< HEAD
-    private fun setupAction() {
-        binding.logoutButton.setOnClickListener {
-            viewModel.logout()
-        }
-        binding.cameraButton.setOnClickListener {
-            startActivity(Intent(this, CameraActivity::class.java))
-        }
-    }
-
-    private fun playAnimation() {
-        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
-            duration = 6000
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.REVERSE
-        }.start()
-
-        val name = ObjectAnimator.ofFloat(binding.nameTextView, View.ALPHA, 1f).setDuration(100)
-        val message = ObjectAnimator.ofFloat(binding.messageTextView, View.ALPHA, 1f).setDuration(100)
-        val logout = ObjectAnimator.ofFloat(binding.logoutButton, View.ALPHA, 1f).setDuration(100)
-        val camera = ObjectAnimator.ofFloat(binding.cameraButton, View.ALPHA, 1f).setDuration(100)
-
-        AnimatorSet().apply {
-            playSequentially(name, message, logout)
-            startDelay = 100
-        }.start()
-    }
-=======
     override fun onBackPressed() {
         super.onBackPressed()
         finishAffinity()
     }
 
->>>>>>> origin/main
 }
